6ac08834c2814e1db12aa19d9c259c05086028ae
rrr1818 <104551077+rrr1818@users.noreply.github.com>
2023-03-23 18:15:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'c2a891d501msh422b9bdfa9f69b6p15caa4jsnd78ec9446aa4',
+    'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+  }
+};
+const getWeather = (city)=>{
+  cityName.innerHTML=` ${city.value}`;
+  console.log(city);
+
+fetch(`https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city.value}`, options)
+  .then(response => response.json())
+  .then((response) => {
+
+
+    console.log(response)
+
+    cloud_pct.innerHTML = ` ${response.cloud_pct}`
+    temp.innerHTML = " " + response.temp
+    temp2.innerHTML = " " + response.temp
+    feels_like.innerHTML = response.feels_like
+    humidity.innerHTML = response.humidity
+    humidity2.innerHTML = response.humidity
+    min_temp.innerHTML = response.min_temp
+    max_temp.innerHTML = response.max_temp
+    wind_speed.innerHTML = response.wind_speed
+    wind_speed2.innerHTML = response.wind_speed
+    wind_degrees.innerHTML = response.wind_degrees
+    sunrise.innerHTML = response.sunrise
+    sunset.innerHTML = response.sunset
+
+
+  })
+  .catch(err => console.error(err));
+}
+document.getElementById("submit").addEventListener("click", (e)=>{
+  e.preventDefault()
+  getWeather(document.getElementById("city"))
+})
+
+//getWeather(city)
